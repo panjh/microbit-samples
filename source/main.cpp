7756ce30a32bd1f48b71862de68a43d060cbc320
Edison <edisons@126.com>
2018-01-03 16:20:38 +0800

    use my own microbit main method

--- source/main.cpp
+++ source/main.cpp
@@ -1,43 +1,36 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2016 British Broadcasting Corporation.
-This software is provided by Lancaster University by arrangement with the BBC.
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-*/
-
 #include "MicroBit.h"
 
 MicroBit uBit;
+static char _out[1024];
+char* volatile out = _out;
 
-int main()
-{
-    // Initialise the micro:bit runtime.
+void run0(int i) {
+	static int p = 45;
+	uBit.io.P13.setServoValue(p);
+	uBit.io.P14.setServoValue(p);
+	uBit.io.P15.setServoValue(p);
+	uBit.io.P16.setServoValue(p);
+	p = 180 - p;
+}
+
+void run1(int i) {
+	int v = uBit.io.P2.getDigitalValue(PullUp);
+	uBit.serial.printf("%d\n", v);
+}
+
+int main() {
     uBit.init();
-
-    // Insert your code here!
-    uBit.display.scroll("HELLO WORLD! :)");
-
-    // If main exits, there may still be other fibers running or registered event handlers etc.
-    // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
-    // sit in the idle task forever, in a power efficient sleep.
-    release_fiber();
+    int _line = 0;
+    for (int i = 0; ; i = (i + 1) % 4) {
+    	if (i == 0) run1(i);
+        if (out != _out) {
+        	uBit.serial.printf("\n%d:\n", ++_line);
+        	*out = 0;
+        	uBit.serial.printf(_out);
+        	out = _out;
+        	uBit.serial.printf("\n");
+        }
+        uBit.sleep(125);
+    }
 }
 
